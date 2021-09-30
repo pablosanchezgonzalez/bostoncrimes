8daee5716b052e5191112d41dce018e84f4ff3aa
Pablo Sanchez Gonzalez <pablo.sanchezgonzalez@altran.es>
2021-09-30 18:12:28 +0200

    Corrección de test de excepción

--- unit_test/test_create_directory_if_not_exists.py
+++ unit_test/test_create_directory_if_not_exists.py
@@ -9,7 +9,8 @@ class TestCreateDirectory(unittest.TestCase):
 
     def test_that_when_input_not_string_then_raises_type_error(self):
 
-        self.assertRaises(TypeError, boston.main.create_directory_if_not_exists, 2)
+        with self.assertRaises(TypeError):
+            boston.main.create_directory_if_not_exists(2)
 
     def test_that_when_called_then_return_is_none(self):
 
