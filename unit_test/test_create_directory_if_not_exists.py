70f28dd4a20047fde04008a6d7b5e0d03e70e316
Pablo Sanchez Gonzalez <pablo.sanchezgonzalez@altran.es>
2021-09-30 10:55:09 +0200

    Test retorno función create directory

--- unit_test/test_create_directory_if_not_exists.py
+++ unit_test/test_create_directory_if_not_exists.py
@@ -5,16 +5,21 @@ import os
 
 class TestCreateDirectory(unittest.TestCase):
     
+    path = "prueba"
+
     def test_creation(self):
         
-        path = "prueba"
 
-        if os.path.isdir(path):
-            os.rmdir(path)
+        if os.path.isdir(self.path):
+            os.rmdir(self.path)
 
-        boston.main.create_directory_if_not_exists(path)
+        boston.main.create_directory_if_not_exists(self.path)
 
-        self.assertTrue(os.path.isdir(path))
+        self.assertTrue(os.path.isdir(self.path))
+
+    def test_return_none(self):
+
+        self.assertEqual(boston.main.create_directory_if_not_exists(self.path), None)
 
 
 if __name__ == '__main__':
